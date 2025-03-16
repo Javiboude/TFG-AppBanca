e01da5ed9f7fe00414229554e6632bb1b39dd803
Javier <javierboudere@gmail.com>
2025-03-16 11:58:07 +0100

    Commit pantalla de carga hecha

--- app/src/main/java/com/example/httpclienttest/MainActivity.kt
+++ app/src/main/java/com/example/httpclienttest/MainActivity.kt
@@ -3,23 +3,22 @@ package com.example.httpclienttest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.material3.Surface
-import com.example.httpclienttest.ui.screens.user.UserListScreen
-import com.example.httpclienttest.ui.screens.user.UserListViewModel
+import com.example.httpclienttest.ui.screens.pantallaDeCarga.PantallaDeCargaScreen
+import com.example.httpclienttest.ui.screens.pantallaDeCarga.PantallaDeCargaViewModel
 import com.example.httpclienttest.ui.theme.HttpClientTestTheme
 
 class MainActivity : ComponentActivity() {
 
-    private val userListViewModel: UserListViewModel by viewModels()
+    private val pantallaDeCargaViewModel: PantallaDeCargaViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             HttpClientTestTheme {
                 Surface {
-                    UserListScreen(userListViewModel)
+                    PantallaDeCargaScreen(pantallaDeCargaViewModel)
                 }
 
             }
