8794dc8006f35a9ce9184b74d6ffdbd920dc7f15
Javier <javierboudere@gmail.com>
2025-03-16 11:58:07 +0100

    Initial commit

---
+++ app/src/main/java/com/example/httpclienttest/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.httpclienttest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.material3.Surface
+import com.example.httpclienttest.ui.screens.user.UserListScreen
+import com.example.httpclienttest.ui.screens.user.UserListViewModel
+import com.example.httpclienttest.ui.theme.HttpClientTestTheme
+
+class MainActivity : ComponentActivity() {
+
+    private val userListViewModel: UserListViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HttpClientTestTheme {
+                Surface {
+                    UserListScreen(userListViewModel)
+                }
+
+            }
+        }
+    }
+}
